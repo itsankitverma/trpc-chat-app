a605e10424108026c169ea97f31465e2ae82dbee
iankitverma <imankit.v5@gmail.com>
2023-01-08 21:44:06 +0530

    feat: added landing page

---
+++ components/landingPage.tsx
@@ -0,0 +1,151 @@
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { signIn } from "next-auth/react";
+
+const navigation = [
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Marketplace", href: "#" },
+  { name: "Company", href: "#" },
+];
+
+export default function LandingPage() {
+  return (
+    <div className="relative overflow-hidden bg-white">
+      <div className="relative pt-6 pb-16 sm:pb-24 lg:pb-32">
+        <main className="mx-auto mt-16 max-w-7xl sm:mt-24 lg:mt-32">
+          <div className="lg:grid lg:grid-cols-12 lg:gap-8">
+            <div className="sm:text-center md:mx-auto md:max-w-2xl lg:col-span-6 lg:text-left">
+              <h1>
+                <span className="block text-base font-semibold text-gray-500 sm:text-lg lg:text-base xl:text-lg">
+                  Coming soon
+                </span>
+                <span className="mt-1 block text-4xl font-bold tracking-tight sm:text-5xl xl:text-6xl">
+                  <span className="block text-gray-900">
+                    Data to enrich your
+                  </span>
+                  <span className="block text-indigo-600">online business</span>
+                </span>
+              </h1>
+              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+                fugiat aliqua ad ad non deserunt sunt.
+              </p>
+              <div className="mt-8 sm:mx-auto sm:max-w-lg sm:text-center lg:mx-0 lg:text-left">
+                <p className="text-base font-medium text-gray-900">
+                  Login to a whole new world
+                </p>
+                <form action="#" method="POST" className="mt-3 sm:flex">
+                  <label htmlFor="email" className="sr-only">
+                    Login to continue
+                  </label>
+
+                  <button
+                    type="submit"
+                    className="mt-3 w-full rounded-md border border-transparent bg-gray-800 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:inline-flex sm:w-auto sm:flex-shrink-0 sm:items-center"
+                    onClick={() => {
+                      signIn();
+                    }}
+                  >
+                    Login
+                  </button>
+                </form>
+              </div>
+            </div>
+            <div className="relative mt-12 sm:mx-auto sm:max-w-lg lg:col-span-6 lg:mx-0 lg:mt-0 lg:flex lg:max-w-none lg:items-center">
+              <svg
+                className="absolute top-0 left-1/2 origin-top -translate-x-1/2 -translate-y-8 scale-75 transform sm:scale-100 lg:hidden"
+                width={640}
+                height={784}
+                fill="none"
+                viewBox="0 0 640 784"
+                aria-hidden="true"
+              >
+                <defs>
+                  <pattern
+                    id="4f4f415c-a0e9-44c2-9601-6ded5a34a13e"
+                    x={118}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  y={72}
+                  width={640}
+                  height={640}
+                  className="text-gray-50"
+                  fill="currentColor"
+                />
+                <rect
+                  x={118}
+                  width={404}
+                  height={784}
+                  fill="url(#4f4f415c-a0e9-44c2-9601-6ded5a34a13e)"
+                />
+              </svg>
+              <div className="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
+                <button
+                  type="button"
+                  className="relative block w-full overflow-hidden rounded-lg bg-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                >
+                  <span className="sr-only">Watch our video to learn more</span>
+                  <img
+                    className="w-full"
+                    src="https://images.unsplash.com/photo-1556740758-90de374c12ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                    alt=""
+                  />
+                  <span
+                    className="absolute inset-0 flex h-full w-full items-center justify-center"
+                    aria-hidden="true"
+                  >
+                    <svg
+                      className="h-20 w-20 text-indigo-500"
+                      fill="currentColor"
+                      viewBox="0 0 84 84"
+                    >
+                      <circle
+                        opacity="0.9"
+                        cx={42}
+                        cy={42}
+                        r={42}
+                        fill="white"
+                      />
+                      <path d="M55.5039 40.3359L37.1094 28.0729C35.7803 27.1869 34 28.1396 34 29.737V54.263C34 55.8604 35.7803 56.8131 37.1094 55.9271L55.5038 43.6641C56.6913 42.8725 56.6913 41.1275 55.5039 40.3359Z" />
+                    </svg>
+                  </span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,54 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Appbar from "../components/appbar/appbar";
+import LandingPage from "../components/landingPage";
 import { api } from "../utils/api";
 
 export default function Index() {
   const router = useRouter();
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
   const usersList = api.user.getAllUser.useQuery();
-
-  const { data } = usersList!;
 
   return (
     <>
       <Appbar />
       <div className="relative flex flex-col items-center justify-center overflow-hidden bg-white">
-        <p>Registered Users</p>
-
-        <>
-          {usersList.data?.map((user) => (
-            <li
-              key={user.email}
-              className="flex cursor-pointer py-4"
-              onClick={() => {
-                router.push(`${router.pathname}/p/${user.handle}`);
-              }}
-            >
-              <img className="h-10 w-10 rounded-full" src={user.image} alt="" />
-              <div className="ml-3">
-                <div className="flex items-center gap-2">
-                  <p className="text-sm font-medium text-gray-900">
-                    {user.name}
-                  </p>
-                  <span>{user?.id === session?.user.id && "(You)"}</span>
-                </div>
-                <p className="text-sm text-gray-500">{user.email}</p>
-              </div>
-            </li>
-          ))}
-        </>
-
-        {/*  */}
+        {status === "authenticated" && (
+          <div>
+            <p>Registered Users</p>
+            <>
+              {usersList.data?.map((user) => (
+                <li
+                  key={user.email}
+                  className="flex cursor-pointer py-4"
+                  onClick={() => {
+                    router.push(`${router.pathname}/p/${user.handle}`);
+                  }}
+                >
+                  <img
+                    className="h-10 w-10 rounded-full"
+                    src={user.image}
+                    alt=""
+                  />
+                  <div className="ml-3">
+                    <div className="flex items-center gap-2">
+                      <p className="text-sm font-medium text-gray-900">
+                        {user.name}
+                      </p>
+                      <span>{user?.id === session?.user.id && "(You)"}</span>
+                    </div>
+                    <p className="text-sm text-gray-500">{user.email}</p>
+                  </div>
+                </li>
+              ))}
+            </>
+          </div>
+        )}
+        {status === "unauthenticated" && (
+          <div>
+            <LandingPage />
+          </div>
+        )}
       </div>
     </>
   );
